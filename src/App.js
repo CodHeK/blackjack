f7d765a6a271b464cd925a50b6ac07f46a179cab
CodHeK <gaganganapathyas@yahoo.in>
2018-08-18 14:50:12 +0530

    player hit added

--- src/App.js
+++ src/App.js
@@ -9,12 +9,28 @@ class App extends Component {
       deck: [],
       player: [],
       dealer: [],
+      playerFinalScore: 0,
+      dealerFinalScore: 0,
     };
   }
 
   startGame() {
-    $(".box").fadeOut();
+    $(".box, .pwins, .news").fadeOut();
     $(".arena").fadeIn(1000);
+  }
+
+  getScore(person) {
+    let sum = 0;
+    for(let p of person) {
+      if(p.name === "A") {
+        if(sum + 11 > 21) {
+          sum += 1;
+          continue;
+        }
+      }
+      sum += (p.val);
+    }
+    return sum;
   }
 
   getCard() {
@@ -56,9 +72,12 @@ class App extends Component {
       let card = this.getCard();
       dealer_copy.push(card);
     }
+    let pfs = this.getScore(player_copy), dfs = this.getScore(dealer_copy);
     this.setState({
       player: player_copy,
       dealer: dealer_copy,
+      playerFinalScore: pfs,
+      dealerFinalScore: dfs,
     });
   }
 
@@ -123,8 +142,51 @@ class App extends Component {
     $(".begins").fadeOut();
   }
 
-  render() {
+  playerHit() {
     const { player, dealer } = this.state;
+    let player_copy = player, dealer_copy = dealer;
+    let card = this.getCard();
+    player_copy.push(card);
+    let pfs = this.getScore(player_copy);
+    this.setState({
+      player: player_copy,
+      playerFinalScore: pfs,
+    }, () => {
+      const { playerFinalScore } = this.state;
+      setTimeout(function() {
+        if(playerFinalScore > 21) {
+          $(".damp").hide();
+          $(".title1").html("PLAYER BUSTED \<br /> DEALER WINS !!");
+          $(".pwins, .news").fadeIn();
+        }
+        else if(playerFinalScore === 21) {
+          $(".damp").hide();
+          $(".title1").html("PLAYER WINS !!");
+          $(".pwins, .news").fadeIn();
+        }
+      }, 1000)
+    });
+  }
+
+  playerStand() {
+
+  }
+
+  refresh() {
+    this.setState({
+      deck: [],
+      player: [],
+      dealer: [],
+      playerFinalScore: 0,
+      dealerFinalScore: 0,
+    }, () => {
+      $(".pwins, .news").fadeOut();
+      $(".damp, .begins").fadeIn();
+    });
+  }
+
+  render() {
+    const { player, dealer, playerFinalScore, dealerFinalScore } = this.state;
     const player_cards = player.map((card) => {
     if(card.type === "B") {
       return <li className="card_B">{card.name}</li>
@@ -148,19 +210,21 @@ class App extends Component {
         </div>
         <div className="arena">
           <h1 className="min-title">Blackjack</h1>
-          <div className="row">
+          <div className="pwins"><h1 className="title1"></h1></div>
+          <button className="btn btn-default begin_game news" onClick={this.refresh.bind(this)}>NEW GAME</button>
+          <div className="row damp">
             <div className="col-md-6 player">
-              <h4 className="player-tag">YOU</h4>
+              <h4 className="player-tag">YOU&nbsp;:&nbsp;{playerFinalScore}</h4>
               <ul className="player_list">{player_cards}</ul>
               <div className="butts">
                 <ul className="butt_list">
-                  <li className="each_butt"><button class="btn btn-default begin_game">HIT</button></li>&nbsp;&nbsp;
-                  <li className="each_butt"><button class="btn btn-default begin_game">STAND</button></li>
+                  <li className="each_butt"><button class="btn btn-default begin_game" onClick={this.playerHit.bind(this)}>HIT</button></li>&nbsp;&nbsp;
+                  <li className="each_butt"><button class="btn btn-default begin_game" onClick={this.playerStand.bind(this)}>STAND</button></li>
                 </ul>
               </div>
             </div>
             <div className="col-md-6 dealer">
-              <h4 className="dealer-tag">DEALER</h4>
+              <h4 className="dealer-tag">DEALER&nbsp;:&nbsp;{dealerFinalScore}</h4>
               <ul className="dealer_list">{dealer_cards}</ul>
             </div>
           </div>
